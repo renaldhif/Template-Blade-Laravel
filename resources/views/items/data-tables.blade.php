fd331c8907d554341828c754a661c12069de942a
Renaldhi Fahrezi <60850559+renaldhif@users.noreply.github.com>
2020-07-02 12:13:19 +0800

    edit source dir

--- resources/views/items/data-tables.blade.php
+++ resources/views/items/data-tables.blade.php
@@ -8,8 +8,8 @@
 
 
 @push('scripts')
-<script src="/plugins/datatables/jquery.dataTables.js')"></script>
-<script src="/plugins/datatables-bs4/js/dataTables.bootstrap4.js')"></script>
+<script src="../../plugins/datatables/jquery.dataTables.js')"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js')"></script>
 <script>
     $(function (){
         $("#example1").DataTable();
